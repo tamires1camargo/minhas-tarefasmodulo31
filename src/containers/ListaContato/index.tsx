9f52c06a30310544e410e0a9e2762c0a19208757
tamires_camargo <tamires.camargo.soares@gmail.com>
2023-08-10 19:32:52 -0300

    ajuste intermediario

--- src/containers/ListaContato/index.tsx
+++ src/containers/ListaContato/index.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux'
 import Card from '../../components/Card'
-import { Container } from './styles'
-import { RootReducer } from '../../store/reducers/contact'
+import { RootReducer } from '../../store'
+import { CardContainer } from '../../components/Card/styles'
 
 const ContactList = () => {
   const { itens } = useSelector((state: RootReducer) => state.contacts)
@@ -10,8 +10,9 @@ const ContactList = () => {
   const filterContacts = () => {
     let filterContacts = itens
     if (term !== undefined) {
-      filterContacts = filterContacts.filter(
-        ({ name }) => name.toLowerCase().search(term.toLocaleLowerCase()) >= 0
+      filterContacts = itens.filter(
+        (item: { nome: string }) =>
+          item.nome.toLowerCase().search(term.toLowerCase()) >= 0
       )
 
       return filterContacts
@@ -23,11 +24,13 @@ const ContactList = () => {
   const contacts = filterContacts()
 
   return (
-    <Container>
-      {contacts.map(({ name, email, tel, id }) => (
-        <Card key={id} name={name} email={email} tel={tel} id={id} />
+    <div>
+      {contacts.map((c) => (
+        <CardContainer key={c.name}>
+          <Card id={c.id} name={c.name} email={c.email} tel={c.telefone} />
+        </CardContainer>
       ))}
-    </Container>
+    </div>
   )
 }
 
